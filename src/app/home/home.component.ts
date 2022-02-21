6ee2f3f458768dc36a5e5ed497dc5a5203dd44cb
manoj1995madushanka <madushankamanoj414@gmail.com>
2022-02-21 22:41:03 +0530

    add login and logout functionality

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
+import {AuthService} from '../auth.service';
 
 @Component({
   selector: 'app-home',
@@ -8,7 +9,7 @@ import {Router} from '@angular/router';
 })
 export class HomeComponent implements OnInit {
 
-  constructor(private router: Router) {
+  constructor(private router: Router, private authService: AuthService) {
   }
 
   ngOnInit() {
@@ -20,5 +21,15 @@ export class HomeComponent implements OnInit {
         allowEdit: '1'
       }, fragment: 'loading'
     });
+  }
+
+  onLogin() {
+    console.log('user login');
+    this.authService.login();
+  }
+
+  onLogout() {
+    console.log('user logout');
+    this.authService.logout();
   }
 }
