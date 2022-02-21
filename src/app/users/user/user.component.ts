2cb3b178efe7beba319e3d872e2e65ed9ccae6ef
manoj1995madushanka <madushankamanoj414@gmail.com>
2022-02-21 10:58:54 +0530

    unsubscribe params subscription when app destroy

--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -1,13 +1,15 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {ActivatedRoute, Params} from '@angular/router';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
-export class UserComponent implements OnInit {
+export class UserComponent implements OnInit, OnDestroy {
   user: { id: number, name: string };
+  paramSubscription: Subscription;
 
   constructor(private route: ActivatedRoute) {
   }
@@ -18,12 +20,16 @@ export class UserComponent implements OnInit {
       name: this.route.snapshot.params['name']
     };
     /** this will get route parameter reactively this will help when we use <a> tag*/
-    this.route.params.subscribe(
+    this.paramSubscription = this.route.params.subscribe(
       (params: Params) => {
         this.user.id = params['id'];
         this.user.name = params['name'];
       }
     );
+  }
+
+  ngOnDestroy(): void {
+    this.paramSubscription.unsubscribe();
   }
 
 }
